0ca92b2bd32d648db240b5eafb9efec6afb8181a
Zoucl26 <124428003+Zoucl26@users.noreply.github.com>
2023-02-06 16:57:19 +0800

    Add files via upload

---
+++ test1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class myInt
+{
+public:
+    myInt(int a ,int b):m_li(a),m_ri(b)
+     {}
+	myInt():m_li(0),m_ri(0)
+     {}
+    void print()
+    {
+        cout<<"m_li = "<<this->m_li<<endl;
+        cout<<"m_ri = "<<this->m_ri<<endl;
+    }
+ 
+    void set(int li, int ri)
+    {
+        this->m_li = li;
+        this->m_ri = ri;
+    }
+private:
+    int m_li;
+    int m_ri;
+    string name;
+};
+ 
+ 
+int main() {
+ 
+	vector<myInt> v1(30);
+	for(vector<myInt>::iterator iter = v1.begin(); iter != v1.end(); iter++)
+    {
+        iter->print();
+    }
+
+
+ 
+	return 0;
+}
